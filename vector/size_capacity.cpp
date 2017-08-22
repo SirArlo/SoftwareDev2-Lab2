96a9aec13c2197dd33a4f32b6cf0cab263f4cdb0
Arlo <eardsa@gmail.com>
2017-08-22 14:24:07 +0200

    Exercise 2.1

--- vector/size_capacity.cpp
+++ vector/size_capacity.cpp
@@ -20,3 +20,9 @@ int main()
 
     return 0;
 }
+
+/*2.1  The size of the vector is defined as the number of elements currently in the vector where the 
+capacity of the vector is the amount of space the vector is currently using in memory, the size of the vector 
+is always equal to or smaller than the capacity of the vector because the vector doubles the capacity 
+every time more space is required to store more elements this is why vectors are said to be able to resize dynamically.
+*/
